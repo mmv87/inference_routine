d56c2e93f6ec5c600ab6c84ca861216eaa24a293
mithun_vmk <mithun.kct@gmail.com>
2026-05-04 21:25:37 -0400

    add

--- modules/ts_encoder.py
+++ modules/ts_encoder.py
@@ -7,60 +7,70 @@ from ts_encoder_rel_bias import PatchTSTEncoder
 from ts_encoder_rel_bias import PatchTSTEncoder"""
 import torch.nn as nn  
 import torch
+#from modules.ts_encoder_perceiver_resampler import PatchTSTEncoder
+device ='cuda' if torch.cuda.is_available() else 'cpu'
+
 class llm_projection(nn.Module):
-    def __init__(self,conv_module,conv_features,trans_module,trans_embedding,d_fusion,d_llm):
+    def __init__(self,trans_module,trans_embedding,d_fusion,d_llm,device=None):
         super().__init__()
-        self.conv_module=conv_module
-        self.trans_module=trans_module
+        self.device =device
+        self.ts_encoder=trans_module
+        
+        self.trans_embedding=trans_embedding #1024
         self.d_fusion=d_fusion ###intermediate dimesnion to 512-->[1024]-->3072
         self.d_llm=d_llm
-        self.conv_features=conv_features
-        self.trans_embedding=trans_embedding
-        #filter based on z_conv
-        self.filter_gen = nn.Sequential(
-            nn.Linear(self.conv_features, self.trans_embedding),
-            nn.LayerNorm(self.trans_embedding),
-            nn.Sigmoid()
-        )
+
         ###multimodal_bridge to project from z_trans to llm_embed
         self.mm_bridge=nn.Sequential(
-            nn.Linear(self.trans_embedding,self.d_fusion),
+            nn.Linear(self.trans_embedding,self.d_fusion,bias=False),
             nn.GELU(),
-            nn.Linear(self.d_fusion,self.d_llm)
+            nn.Linear(self.d_fusion,self.d_llm,bias=False)
         )
         self.norm_projection=nn.LayerNorm(self.d_llm)
         
-    def forward(self,x):
-        conv_embed= self.conv_module(x)
-        trans_embed=self.trans_module(x)
-        ##z_conv=self.conv_proj(conv_embed)
-        ##z_conv to create a mask.
-        z_mask=self.filter_gen(conv_embed)
-        ##print(z_mask.shape)
-        z_gated=z_mask*trans_embed
-        z_proj=self.mm_bridge(z_gated)
+    def forward(self,x,ch_mask=None):
+        ts_features= self.ts_encoder(x,ch_mask)
+        ts_features.to(self.device)
+        
+        z_proj=self.mm_bridge(ts_features)
         ##z_gated=z_trans*z_mask
         ##layer norm
         z_llm = self.norm_projection(z_proj)
+
         return z_llm
+"""
+ts_test=torch.randn(1,1,1014)
+actual_ch=ts_test.shape[1]
+max_ch=21
+lat_q_dim=5
+bool_mask=torch.zeros(max_ch*lat_q_dim,dtype=torch.bool)
+token_idx=torch.arange(actual_ch*lat_q_dim)
+bool_mask[token_idx]=True
+print(f'bolean_shape:{bool_mask.shape}')
+bool_mask=bool_mask.unsqueeze(-1)
+bool_mask_batch = torch.stack([bool_mask],dim=0)
+print(bool_mask_batch)
+
+conv_layers_1=[(64,7,3,1),(128,5,3,2),(256,3,2,2),(512,3,2,2),(1024,3,2,2)]
+ts_encoder= PatchTSTEncoder(conv_layers_1,1024,max_ch=21,n_layers=1,d_model=256,n_heads=2,d_ff=256,bias=False,lat_dim=5,
+                 dropout=0.1,activation='gelu',pre_norm=False)
+
+ts_encoder = llm_projection(ts_encoder,1024,2048,3072,device=device)
+ts_embeddings=ts_encoder(ts_test,ch_mask=bool_mask_batch)
+
+print(ts_embeddings.shape)
+
+ts_embedding_new=torch.narrow(ts_embeddings,1,0,actual_ch*lat_q_dim)
+print(ts_embedding_new)"""
+##print(ts_embeddings[:,:7,:5])
 
 """
-ts_text=torch.randn(1,3,2,128)
-conv_layers = [(128,5,1),(64,3,1)]
-patch_len=128
-ts_transformer=PatchTSTEncoder(patch_len=patch_len,n_layers=2,d_model=512,n_heads=8,
-                             shared_embedding=True,d_ff=1024,norm='Layer',attn_dropout=0.,dropout=0.1,activation='gelu',store_attn=False,res_attention=False,pre_norm=True,pe='zeros',learn_pe=True,verbose=False)
-ts_conv_module=ConvFeatureExtraction(conv_layers,dropout=0.1)
-
-ts_encoder = llm_projection(ts_conv_module,64,ts_transformer,512,1024,3072)
-"
 for param_tensor in ts_encoder.state_dict():
     if 'trans' in param_tensor:
         print(f'{param_tensor} :{ts_encoder.state_dict()[param_tensor].size()}')
     else:
-        continue 
+        continue """
+        
 
-ts_embeddings=ts_encoder(ts_text)
-print(ts_embeddings.shape)"""
 
 
